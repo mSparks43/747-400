fecfdb5e171aaacfb45d5c7cd6aba1cd6f0477e1
MisterFahrenhiet <62818033+MisterFahrenhiet@users.noreply.github.com>
2020-12-25 14:32:29 -0500

    Update B744.fms.pages.options.lua

--- plugins/xtlua/scripts/B747.68.xt.fms/activepages/B744.fms.pages.options.lua
+++ plugins/xtlua/scripts/B747.68.xt.fms/activepages/B744.fms.pages.options.lua
@@ -3,7 +3,7 @@ fmsPages["ACMS"].getPage=function(self,pgNo,fmsID)
 
   return {
             
-"   SIMULATION OPTIONS   ",
+"    SIM OPTIONS INDEX   ",
 "                        ",
 "<ENGINES                ",
 "                        ",
@@ -21,3 +21,54 @@ end
 fmsFunctionsDefs["OPTIONS"]["L1"]={"setpage","CONFIG-ENGINES"}
 fmsFunctionsDefs["OPTIONS"]["L2"]={"setpage","CONFIG-SOUNDS"}
 fmsFunctionsDefs["OPTIONS"]["L6"]={"setpage","INDEX"}
+
+fmsPages["CONFIG-ENGINES"]=createPage("CONFIG-ENGINES")
+fmsPages["CONFIG-ENGINES"].getPage=function(self,pgNo,fmsID)
+
+  return {
+            
+"     ENGINE OPTIONS     ",
+"ENGINE MODEL            ",
+"GE/PW/RR                ",
+"                        ",
+"                        ",
+"                        ",
+"                        ",
+"                        ",
+"                        ",
+"                        ",
+"                        ", 
+"------------------------",
+"<INDEX                  "
+    }
+end
+
+fmsFunctionsDefs["CONFIG-SOUNDS"]["L6"]={"setpage","OPTIONS"}
+
+fmsPages["CONFIG-SOUNDS"]=createPage("CONFIG-SOUNDS")
+fmsPages["CONFIG-SOUNDS"].getPage=function(self,pgNo,fmsID)
+  
+  return {
+            
+"     SOUND OPTIONS      ",
+"                        ",
+"<GENERAL SOUND INFO     ",
+"                        ",
+"<COCKPIT                ",
+"                        ",
+"<CABIN                  ",
+"                        ",
+"<GPWS                   ",
+"                        ",
+"<SWITCHES               ", 
+"------------------------",
+"<INDEX                  "
+    }
+end
+
+fmsFunctionsDefs["CONFIG-SOUNDS"]["L1"]={"setpage","SOUND-INFO"}
+fmsFunctionsDefs["CONFIG-SOUNDS"]["L2"]={"setpage","SOUND-COCKPIT"}
+fmsFunctionsDefs["CONFIG-SOUNDS"]["L3"]={"setpage","SOUND-CABIN"}
+fmsFunctionsDefs["CONFIG-SOUNDS"]["L4"]={"setpage","SOUND-GPWS"}
+fmsFunctionsDefs["CONFIG-SOUNDS"]["L5"]={"setpage","SOUND-SWITCHES"}
+fmsFunctionsDefs["CONFIG-SOUNDS"]["L6"]={"setpage","OPTIONS"}
